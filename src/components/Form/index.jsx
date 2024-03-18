5a93c05b0426f49c189023d08126af217179df02
Felipe Valcarenghi Lippert <felipe.v.lippert@gmail.com>
2024-03-18 21:16:57 +1100

    finished

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,56 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Form = () => {
-    let [weight, setWeight] = useState(0);
-    let [height, setHeight] = useState(0);
-    let bmi = weight/ (height*height);
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [bmi, setBmi] = useState(0);
+
+
+    const calcBmi = () => {
+        const bmiValue = (weight / (height * height));
+        setBmi(bmiValue.toFixed(1));
+    }
     
-
-
-    const returnBmi = () => {
-        
+    function returnBmi(bmi) {
         if (bmi <= 18.5) {
             return (
-                <p>Your BMI is: {bmi.toFixed(1)}, categorized as underweight</p>
+                <p>Your BMI is: {bmi}, categorized as underweight</p>
                 )
             } 
         else if (bmi <= 24.9){
             return (
-                <p>Your BMI is: {bmi.toFixed(1)}, categorized as normal</p>
+                <p>Your BMI is: {bmi}, categorized as normal</p>
                 )
             }
             else if (bmi <= 29.9){
                 return (
-                    <p>Your BMI is: {bmi.toFixed(1)}, categorized as overweight</p>
+                    <p>Your BMI is: {bmi}, categorized as overweight</p>
                     )
                 }
             else if (bmi >= 30) {
                 return (
-                    <p>Your BMI is: {bmi.toFixed(1)}, categorized as obese</p>
+                    <p>Your BMI is: {bmi}, categorized as obese</p>
                     )
             }
     }
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        returnBmi()
-        
-    
+        calcBmi();
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <div className="container">
+            <form onSubmit={handleSubmit}>
             <h3>Let's determine your Body Mass Index (BMI)</h3>
             <input onChange={e => setWeight(e.target.value) } type="text" placeholder="Your weight" />
             <input onChange={e => setHeight(e.target.value)} type="text" placeholder="Your height (in m)" />
-            <button type="submit">Calculate</button>
-            
-
-
-
-
-        </form>
+            <button onClick={() => returnBmi} type="submit">Calculate</button>
+            {bmi != 0 && (
+                returnBmi(bmi)
+            )}
+            </form>
+        </div>
+        
     )
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import './header.css'
 const Header = () => {
     return (
 
-        <header>
+        <header className='container'>
             <img src="https://www.heartfoundation.org.au/getmedia/76ffe0f6-90ff-427d-aecc-b2e7a8bf776a/BMI-calculator.jpg" alt="" />
             <h1>BMI Calculator</h1>
         </header>
